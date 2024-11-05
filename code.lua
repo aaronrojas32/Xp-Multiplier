c158fb0369a830914166423bbc5fe167e1a6d12e
aaronrojas32 <56888910+aaronrojas32@users.noreply.github.com>
2024-11-05 10:49:20 +0100

    Update code.lua

---
+++ code.lua
@@ -0,0 +1,6 @@
+function ExperienceManager:mission_xp()
+	local total_xp = self._global.mission_xp_total and Application:digest_value(self._global.mission_xp_total, false) or 0
+	local current_xp = self._global.mission_xp_current and Application:digest_value(self._global.mission_xp_current, false) or 0
+
+	return (total_xp + current_xp) * 2
+end
