7a334ee112d1c2a114e180a78c588f67367d7331
aaronrojas32 <56888910+aaronrojas32@users.noreply.github.com>
2024-11-06 11:00:04 +0100

    Updated mod

--- code.lua
+++ code.lua
@@ -1,13 +1,10 @@
-local function get_multiplier()
-    local mod = BLT.Mods:GetMod("XP Multiplier")
-    return mod and mod:GetOption("xp_multiplier") or 2 
-end
+--Change this value to select the multiplier
+multiplier = 2
 
 function ExperienceManager:mission_xp()
     local multiplier = get_multiplier()
     local total_xp = self._global.mission_xp_total and Application:digest_value(self._global.mission_xp_total, false) or 0
     local current_xp = self._global.mission_xp_current and Application:digest_value(self._global.mission_xp_current, false) or 0
 
-    -- Aplicar el multiplicador de experiencia
     return (total_xp + current_xp) * multiplier
 end
